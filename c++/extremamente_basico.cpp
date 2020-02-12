cd7b5940cb584c2a994bdf65fb5ceb4d81cf4432
Diego José da Silva <diegojsilvabr@gmail.com>
2020-02-11 23:25:30 -0300

    add second algorithm

---
+++ c++/extremamente_basico.cpp
@@ -0,0 +1,23 @@
+// problem 1001
+//
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+  int a, b, soma;
+  
+  cout << "";
+  cin >> a;
+  cout << "";
+  cin >> b;
+  
+  soma = a + b;
+  
+  cout << "X = " << soma << endl;
+  
+  return 0;
+  
+}
+

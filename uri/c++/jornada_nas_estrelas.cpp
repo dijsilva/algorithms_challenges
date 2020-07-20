6e14dd9f98894a4f0191d03acaf7f093e8a487a0
Diego José da Silva <diegojsilvabr@gmail.com>
2020-07-20 20:32:22 -0300

    jornada nas estrelas

--- uri/c++/jornada_nas_estrelas.cpp
+++ uri/c++/jornada_nas_estrelas.cpp
@@ -1,18 +1,38 @@
 #include <iostream>
-#include <sstream>
-#include <vector>
-#include <string>
+
 using namespace std;
 
-int main () {
-    int numberOfStars;
-    string numberOfAnimals;
+int main(){
 
-    cout << "";
-    cin >> numberOfStars;
 
-    cout << "";
-    getline(cin, numberOfAnimals);
+    ios_base::sync_with_stdio(false);
 
-    cout << numberOfAnimals << endl;
+    int n;
+    long long total_stolen = 0, attacked = 0, qtd_stolen, sum_stolen;
+
+    cin >> n;
+    int stolens[n];
+    int total_stars [n];
+
+    for (int i = 0; i < n; i++){
+        cin >> stolens[i];
+        total_stolen += stolens[i];
+        total_stars[i] = 0;
+    }
+
+    int i = 0;
+    while(i >= 0 && i < n){
+        if (!total_stars[i]) total_stars[i] = 1, attacked++;
+        if (stolens[i] > 0){
+            if(stolens[i] % 2 == 0)
+                stolens[i]--, total_stolen--, i--;
+            else
+                stolens[i]--, total_stolen--, i++;
+        }
+        else
+            i--;
+    }
+    
+    cout << attacked << " " << total_stolen << endl;
+    return 0;
 }
